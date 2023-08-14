9813538046893e43a4fd8ab6cc456412dd8edab6
Julian Thijssen <julianthijssen@gmail.com>
2023-08-14 13:05:23 +0200

    Move direction computations to separate files

--- src/ScatterplotPlugin.cpp
+++ src/ScatterplotPlugin.cpp
@@ -22,6 +22,7 @@
 #include "Compute/LocalDimensionality.h"
 #include "Compute/RandomWalks.h"
 #include "Compute/DataTransformations.h"
+#include "Compute/Directions.h"
 #include "IO/RankingExport.h"
 #include "IO/FloodNodeExport.h"
 #include "Timer.h"
@@ -49,58 +50,6 @@ Q_PLUGIN_METADATA(IID "nl.biovault.GradientExplorerPlugin")
 
 using namespace hdps;
 using namespace hdps::util;
-
-namespace
-{
-    void computeDirection(DataMatrix& dataMatrix, DataMatrix& projMatrix, KnnGraph& knnGraph, int numSteps, std::vector<Vector2f>& directions)
-    {
-        for (int p = 0; p < dataMatrix.rows(); p++)
-        {
-            FloodFill directionsFloodFill(numSteps);
-            directionsFloodFill.compute(knnGraph, p);
-
-            int depth = 3;
-            bigint numNodes = 0;
-            for (int i = 0; i < depth; i++)
-            {
-                numNodes += directionsFloodFill.getWaves()[i].size();
-            }
-
-            Eigen::MatrixXf nodeLocations(numNodes, 2);
-            int n = 0;
-            for (int i = 0; i < depth; i++)
-            {
-                for (int j = 0; j < directionsFloodFill.getWaves()[i].size(); j++)
-                {
-                    int index = directionsFloodFill.getWaves()[i][j];
-
-                    nodeLocations(n, 0) = projMatrix(index, 0);
-                    nodeLocations(n, 1) = projMatrix(index, 1);
-                    n++;
-                }
-            }
-
-            //
-            // Mean centering data.
-            Eigen::MatrixXf centered = nodeLocations.rowwise() - nodeLocations.row(0);
-            // Compute the covariance matrix.
-            Eigen::MatrixXf cov = centered.adjoint() * centered;
-            cov = cov / (nodeLocations.rows() - 1);
-            Eigen::SelfAdjointEigenSolver<Eigen::MatrixXf> eig(cov);
-            // Normalize eigenvalues to make them represent percentages.
-            Eigen::MatrixXf evecs = eig.eigenvectors();
-            // Get the two major eigenvectors and omit the others.
-            Vector2f majorEigenVector;
-            if (eig.eigenvalues()(0) > eig.eigenvalues()(1))
-                majorEigenVector.set(evecs(0, 0), evecs(1, 0));
-            else
-                majorEigenVector.set(evecs(0, 1), evecs(1, 1));
-
-            directions.push_back(Vector2f(projMatrix(p, 0), projMatrix(p, 1)));
-            directions.push_back(majorEigenVector);
-        }
-    }
-}
 
 ScatterplotPlugin::ScatterplotPlugin(const PluginFactory* factory) :
     ViewPlugin(factory),
